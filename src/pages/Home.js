6d95d9af6b95c6de8ce0d00148190836056318be
Subtil-r <subtilrenato@gmail.com>
2021-09-14 17:12:34 +0200

    fixed form displaying

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = ({userData}) => {
     <div className="home_container">
       <img src='https://www.freelogodesign.org/manager/showcase/27b9b99934b6420badf523e04480077a' alt="quiz game logo" />
       <p>You’re not a video game expert until you’ve successfully answered this video game trivia. If you think you’re a real gamer, it’s time for the ultimate test. </p>
-      <UserForm  style={{position: 'relative', backroundColor: "blue"}} submitForm={submitForm} userData={userData} />
+      <UserForm  submitForm={submitForm} userData={userData} />
     </div>
   )
 }
